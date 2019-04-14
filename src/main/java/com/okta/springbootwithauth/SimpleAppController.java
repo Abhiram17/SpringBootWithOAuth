6c2c124dc3ba2e956597f977c34799128c0e6e1f
Abhiram Raghunath M <abhiram@Abhirams-Air.home>
2019-04-13 23:38:16 -0400

    new files, working code

---
+++ src/main/java/com/okta/springbootwithauth/SimpleAppController.java
@@ -0,0 +1,17 @@
+package com.okta.springbootwithauth;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+class SimpleAppController {
+    
+    @RequestMapping("/")
+    String home() {
+        return "home";
+    }
+    @RequestMapping("/restricted")  
+    String restricted() {  
+        return "restricted";  
+    }  
+}
